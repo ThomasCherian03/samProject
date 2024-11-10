bbc9a8fe3d9f25e35b42a9684ee358125eec2ed8
ThomasCherian03 <gotothomas.1523@gmail.com>
2024-11-11 01:01:04 +0530

    Added functionality to update locals.tf

--- controller/portController.js
+++ controller/portController.js
@@ -1,9 +1,14 @@
-import network from "../model/portModel.js";  
+import network from "../model/portModel.js";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const addData = async (req, res) => {
   try {
     const { containers } = req.body;
-
 
     if (!containers || !Array.isArray(containers)) {
       return res.status(400).json({ message: "Containers must be an array" });
@@ -12,13 +17,32 @@ export const addData = async (req, res) => {
     const newNetwork = new network({ containers });
     await newNetwork.save();
 
-    res.status(201).json({ message: "Data added successfully", data: newNetwork });
+    const { containerImage, containerName, containerPort } = containers[0];  // aik controller ka idea karh ke kiya
+
+    const localsFilePath = path.join(__dirname, "../locals.tf");  
+
+    fs.readFile(localsFilePath, 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).json({ message: "Error reading locals.tf file", error: err });
+      }
+
+      let updatedData = data.replace(/ecs-container-image\s*=\s*""/g, `ecs-container-image = "${containerImage}"`);
+      updatedData = updatedData.replace(/ecs-container-name\s*=\s*""/g, `ecs-container-name = "${containerName}"`);
+      updatedData = updatedData.replace(/ecs-container-port\s*=\s*""/g, `ecs-container-port = "${containerPort}"`);
+
+      fs.writeFile(localsFilePath, updatedData, 'utf8', (err) => {
+        if (err) {
+          return res.status(500).json({ message: "Error writing to locals.tf", error: err });
+        }
+
+        res.status(201).json({ message: "Data added and locals.tf updated successfully", data: newNetwork });
+      });
+    });
+
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
-
-
 
 export const viewData = async (req, res) => {
   try {
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import portRouter from "./router/portRouter.js";  
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();  
 
